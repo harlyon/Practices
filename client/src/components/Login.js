740192fdc14f0d460ac73e3a73d1b33945471c45
harlyon <harrisonekpobimi@gmail.com>
2019-04-05 16:35:45 +0200

    fixes

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import Navbar from './Navbar';
 
 class Login extends Component {
+  state = {
+    email: '',
+    password: ''
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+    console.log(e.target.value)
+  }
+
+  submitForm = e => {
+    e.preventDefault();
+    e.target.name += " was-validated";
+  }
+
+  validate = () => {
+
+  }
+
   render() {
     return (
       <div>
@@ -14,16 +33,28 @@ class Login extends Component {
               <div className="card padding-card no-radius border t-align-l">
                 <div className="card-body">
                   <h5 className="card-title mb-4">Login</h5>
-                  <form>
+                  <form  onSubmit={this.submitForm}>
                     <div className="form-group">
                       <label>Email Address <span className="text-danger">*</span></label>
-                      <input type="email" className="form-control" placeholder="Enter Email Address" />
+                        <input type="text"
+                            name="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                            className="form-control"
+                            placeholder="Enter Email Address" />
+                        {/* <div className="errorMsg">{this.state.errors.emailid}</div> */}
                     </div>
                     <div className="form-group">
                       <label>Password <span className="text-danger">*</span></label>
-                      <input type="email" className="form-control" placeholder="Enter Password" />
+                        <input type="password"
+                             name="password"
+                             value={this.state.password}
+                             onChange={this.handleChange}
+                             className="form-control"
+                             placeholder="Enter Password" />
+                        {/* <div className="errorMsg">{this.state.errors.password}</div> */}
                     </div>
-                    <button type="submit" className="btn btn-primary btn-lg btn-block">Login</button>
+                    <button type="submit" className="btn btn-primary btn-lg btn-block" value="SUbmit">Login</button>
                   </form>
                 </div>
               </div>
